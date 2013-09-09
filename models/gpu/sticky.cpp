05e70ea3a4376798ebde77b450dd712dc3477532
Hammad Mazhar <hammad.maz@gmail.com>
2013-09-08 21:44:41 -0500

    added a monkey, that is all

--- models/gpu/sticky.cpp
+++ models/gpu/sticky.cpp
@@ -6,7 +6,7 @@ real gravity = -9.80665;
 real timestep = .0005;
 real seconds_to_simulate = 5;
 
-int max_iter = 20;
+int max_iter = 10;
 
 int num_steps = seconds_to_simulate / timestep;
 
@@ -15,7 +15,7 @@ real container_thickness = .2;
 real container_height = 0;
 real container_friction = 1;
 
-real particle_radius = .025;
+real particle_radius = .01;
 real particle_mass = .05;
 real particle_density = .5;
 real particle_friction = 0;
@@ -40,7 +40,7 @@ void RunTimeStep(T* mSys, const int frame) {
 }
 
 int main(int argc, char* argv[]) {
-	omp_set_num_threads(7);
+	omp_set_num_threads(8);
 	if (argc == 2) {
 		stream = atoi(argv[1]);
 	}
@@ -64,7 +64,7 @@ int main(int argc, char* argv[]) {
 	((ChLcpSolverGPU *) (system_gpu->GetLcpSolverSpeed()))->SetCompliance(0, 0, .0);
 	((ChLcpSolverGPU *) (system_gpu->GetLcpSolverSpeed()))->SetContactRecoverySpeed(300);
 	((ChLcpSolverGPU *) (system_gpu->GetLcpSolverSpeed()))->SetSolverType(ACCELERATED_PROJECTED_GRADIENT_DESCENT);
-	((ChLcpSolverGPU *) (system_gpu->GetLcpSolverSpeed()))->SetWarmStart(true);
+	((ChLcpSolverGPU *) (system_gpu->GetLcpSolverSpeed()))->SetWarmStart(false);
 	((ChCollisionSystemGPU *) (system_gpu->GetCollisionSystem()))->SetCollisionEnvelope(particle_radius * .1);
 	mcollisionengine->setBinsPerAxis(R3(50, 50, 50));
 	mcollisionengine->setBodyPerBin(200, 100);
@@ -119,14 +119,25 @@ int main(int argc, char* argv[]) {
 	AddCollisionGeometry(F, BOX, Vector(container_size.x, container_size.y, container_thickness), Vector(0, 0, 0), Quaternion(1, 0, 0, 0));
 	AddCollisionGeometry(B, BOX, Vector(container_size.x, container_size.y, container_thickness), Vector(0, 0, 0), Quaternion(1, 0, 0, 0));
 	AddCollisionGeometry(Bottom, BOX, Vector(container_size.x, container_thickness, container_size.z), Vector(0, 0, 0), Quaternion(1, 0, 0, 0));
+
 	AddCollisionGeometry(Top, BOX, Vector(container_size.x, container_thickness, container_size.z), Vector(0, 0, 0), Quaternion(1, 0, 0, 0));
 
-	FinalizeObject(L, (ChSystemGPU *) system_gpu);
-	FinalizeObject(R, (ChSystemGPU *) system_gpu);
-	FinalizeObject(F, (ChSystemGPU *) system_gpu);
-	FinalizeObject(B, (ChSystemGPU *) system_gpu);
-	FinalizeObject(Bottom, (ChSystemGPU *) system_gpu);
-	FinalizeObject(Top, (ChSystemGPU *) system_gpu);
+//	FinalizeObject(L, (ChSystemGPU *) system_gpu);
+//	FinalizeObject(R, (ChSystemGPU *) system_gpu);
+//	FinalizeObject(F, (ChSystemGPU *) system_gpu);
+//	FinalizeObject(B, (ChSystemGPU *) system_gpu);
+//	FinalizeObject(Bottom, (ChSystemGPU *) system_gpu);
+//	FinalizeObject(Top, (ChSystemGPU *) system_gpu);
+
+
+	Quaternion quat;
+	quat.Q_from_AngAxis(90,Vector(0,1,0));
+
+	ChSharedBodyPtr Monkey = ChSharedBodyPtr(new ChBody(new ChCollisionModelGPU));
+	InitObject(Monkey, 100000, Vector(0,-2,0), quat, material, true, true, -20, -20);
+	AddCollisionGeometryTriangleMesh(Monkey, "monkey.obj", Vector(0, 0, 0), Quaternion(1, 0, 0, 0));
+	FinalizeObject(Monkey, (ChSystemGPU *) system_gpu);
+
 
 //		ifstream ifile("bunny_init.txt");
 //		string temp;
@@ -165,7 +176,7 @@ int main(int argc, char* argv[]) {
 	layer_gen.material->SetCohesion(0);
 	layer_gen.material->SetCompliance(0);
 	//layer_gen.SetNormalDistribution(rad.x, rad.x/4.0);
-	layer_gen.UseNormalCohesion(2.5, 2.5/2.0);
+	layer_gen.UseNormalCohesion(1, 10);
 	layer_gen.addSnowball(R3(2, -2, 0),SPHERE,1,R3(-22, 0, 0));
 	//layer_gen.addPerturbedVolume(R3(0 + container_pos.x, container_pos.y, 0 + container_pos.z), SPHERE, I3(num_per_dir.x, 1, num_per_dir.z), R3(.1, .1, .1), R3(0, 0, 0), false);
 	//layer_gen.addPerturbedVolume(R3(2, -2, 0), SPHERE, num_per_dir, R3(.1, .1, .1), R3(-22, 0, 0), false);
@@ -175,14 +186,14 @@ int main(int argc, char* argv[]) {
 //	}
 //=========================================================================================================
 //Rendering specific stuff:
-	ChOpenGLManager * window_manager = new ChOpenGLManager();
-	ChOpenGL openGLView(window_manager, system_gpu, 800, 600, 0, 0, "Test_Solvers");
-	openGLView.render_camera->camera_pos = Vector(0, -5, -10);
-	openGLView.render_camera->look_at = Vector(0, -5, 0);
-	openGLView.render_camera->mScale = .5;
-	openGLView.SetCustomCallback(RunTimeStep);
-	openGLView.StartSpinning(window_manager);
-	window_manager->CallGlutMainLoop();
+//	ChOpenGLManager * window_manager = new ChOpenGLManager();
+//	ChOpenGL openGLView(window_manager, system_gpu, 800, 600, 0, 0, "Test_Solvers");
+//	openGLView.render_camera->camera_pos = Vector(0, -5, -10);
+//	openGLView.render_camera->look_at = Vector(0, -5, 0);
+//	openGLView.render_camera->mScale = .5;
+//	openGLView.SetCustomCallback(RunTimeStep);
+//	openGLView.StartSpinning(window_manager);
+//	window_manager->CallGlutMainLoop();
 //=========================================================================================================
 	int file = 0;
 	for (int i = 0; i < num_steps; i++) {

c2486100aa8bdd7f949f981f37173a5ec0746b9d
Hammad Mazhar <hammad.maz@gmail.com>
2013-09-28 23:06:51 -0500

    time step was too large

--- models/gpu/extruder.cpp
+++ models/gpu/extruder.cpp
@@ -3,8 +3,8 @@
 #include "../../common/parser.h"
 #include "../../common/input_output.h"
 real gravity = -9.80665;
-real timestep = .001;
-real seconds_to_simulate = 60;
+real timestep = .0005;
+real seconds_to_simulate = 30;
 
 int max_iter = 30;
 

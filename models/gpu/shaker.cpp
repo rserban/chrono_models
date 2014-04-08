b00ebfb51d06396d28e01ce99433032ce600a760
Hammad Mazhar <hammad.maz@gmail.com>
2014-04-08 13:38:45 -0500

    Update shaker and add it back into Cmake

--- models/gpu/shaker.cpp
+++ models/gpu/shaker.cpp
@@ -30,7 +30,7 @@ double H = P * PI / 6.0 * (pow(D, 3) / pow(L, 2)) / phi;
 double frequency = fstar / sqrtf(H / fabs(gravity));
 double amplitude = Gamma * fabs(gravity) * pow(PI, -0.2e1) * pow(frequency, -0.2e1) / 0.4e1;
 
-int max_iter = 80;
+int max_iter = 30;
 string data_folder = "data/shaker2";
 int num_steps = seconds_to_simulate / timestep;
 
@@ -43,54 +43,46 @@ void RunTimeStep(T* mSys, const int frame) {
 }
 
 int main(int argc, char* argv[]) {
-	bool warm_start = false;
-	if (argc == 7) {
-		//omp_set_num_threads(atoi(argv[1]));
+	if (argc == 5) {
 
-		fstar = atof(argv[2]);
-		Gamma = atof(argv[3]);
+		fstar = atof(argv[1]);
+		Gamma = atof(argv[2]);
 
 		H = P * PI / 6.0 * (pow(D, 3) / pow(L, 2)) / phi;
 		frequency = fstar / sqrtf(H / fabs(gravity));
 		amplitude = Gamma * fabs(gravity) * pow(PI, -0.2e1) * pow(frequency, -0.2e1) / 0.4e1;
 		cout << "Freq: = " << frequency << " Amplitude: = " << amplitude << endl;
-		seconds_to_simulate = atof(argv[4]);
+		seconds_to_simulate = atof(argv[3]);
 		num_steps = seconds_to_simulate / timestep;
-		data_folder = argv[5];
-
-		warm_start = atoi(argv[6]);
-
-	} else {
-		omp_set_num_threads(1);
+		data_folder = argv[4];
 	}
 
 	//=========================================================================================================
-	ChSystemGPU * system_gpu = new ChSystemGPU;
-	ChCollisionSystemGPU *mcollisionengine = new ChCollisionSystemGPU();
+	ChSystemParallelDVI * system_gpu = new ChSystemParallelDVI;
 	system_gpu->SetIntegrationType(ChSystem::INT_ANITESCU);
 
 	//=========================================================================================================
 	system_gpu->SetMaxiter(max_iter);
 	system_gpu->SetIterLCPmaxItersSpeed(max_iter);
-	((ChLcpSolverGPU *) (system_gpu->GetLcpSolverSpeed()))->SetMaxIteration(max_iter);
+	//((ChLcpSolverParallelDVI *) (system_gpu->GetLcpSolverSpeed()))->SetMaxIteration(max_iter);
+	((ChLcpSolverParallelDVI *) (system_gpu->GetLcpSolverSpeed()))->SetMaxIterationNormal(max_iter * 2);
+	((ChLcpSolverParallelDVI *) (system_gpu->GetLcpSolverSpeed()))->SetMaxIterationSliding(max_iter);
+	((ChLcpSolverParallelDVI *) (system_gpu->GetLcpSolverSpeed()))->SetMaxIterationSpinning(0);
 	system_gpu->SetTol(tolerance);
 	system_gpu->SetTolSpeeds(tolerance);
-	((ChLcpSolverGPU *) (system_gpu->GetLcpSolverSpeed()))->SetTolerance(tolerance);
-	((ChLcpSolverGPU *) (system_gpu->GetLcpSolverSpeed()))->SetCompliance(0, 0, 0);
-	((ChLcpSolverGPU *) (system_gpu->GetLcpSolverSpeed()))->SetContactRecoverySpeed(ceil(2 * PI * frequency * amplitude));     //IMPORTANT: this is the max velocity of the plate!!
-	((ChLcpSolverGPU *) (system_gpu->GetLcpSolverSpeed()))->SetSolverType(APGDRS);
-	((ChLcpSolverGPU *) (system_gpu->GetLcpSolverSpeed()))->SetWarmStart(warm_start);
-	((ChCollisionSystemGPU *) (system_gpu->GetCollisionSystem()))->SetCollisionEnvelope(particle_radius * .05);
-	mcollisionengine->setBinsPerAxis(R3(num_per_dir.x * 2, num_per_dir.y * 2, num_per_dir.z * 2));
-	mcollisionengine->setBodyPerBin(100, 50);
+	((ChLcpSolverParallelDVI *) (system_gpu->GetLcpSolverSpeed()))->SetTolerance(tolerance);
+	((ChLcpSolverParallelDVI *) (system_gpu->GetLcpSolverSpeed()))->SetCompliance(0);
+	((ChLcpSolverParallelDVI *) (system_gpu->GetLcpSolverSpeed()))->SetContactRecoverySpeed(ceil(2 * PI * frequency * amplitude));     //IMPORTANT: this is the max velocity of the plate!!
+	((ChLcpSolverParallelDVI *) (system_gpu->GetLcpSolverSpeed()))->SetSolverType(APGDRS);
+	((ChLcpSolverParallelDVI *) (system_gpu->GetLcpSolverSpeed()))->SetWarmStart(false);
+	((ChCollisionSystemParallel *) (system_gpu->GetCollisionSystem()))->SetCollisionEnvelope(particle_radius * .05);
+	((ChCollisionSystemParallel *) (system_gpu->GetCollisionSystem()))->setBinsPerAxis(I3(num_per_dir.x / 2, num_per_dir.y / 2, num_per_dir.z / 2));
+	((ChCollisionSystemParallel *) (system_gpu->GetCollisionSystem()))->setBodyPerBin(100, 50);
 	system_gpu->Set_G_acc(ChVector<>(0, gravity, 0));
 	system_gpu->SetStep(timestep);
 	//=========================================================================================================
 
-	ChSharedBodyPtr L = ChSharedBodyPtr(new ChBody(new ChCollisionModelParallel));
-	ChSharedBodyPtr R = ChSharedBodyPtr(new ChBody(new ChCollisionModelParallel));
-	ChSharedBodyPtr F = ChSharedBodyPtr(new ChBody(new ChCollisionModelParallel));
-	ChSharedBodyPtr B = ChSharedBodyPtr(new ChBody(new ChCollisionModelParallel));
+
 	Bottom = ChSharedBodyPtr(new ChBody(new ChCollisionModelParallel));
 
 	ChSharedPtr<ChMaterialSurface> material;
@@ -99,10 +91,6 @@ int main(int argc, char* argv[]) {
 	material->SetCompliance(0);
 	material->SetCohesion(0);
 
-	InitObject(L, 100000, Vector(-container_size.x + container_thickness, container_height - container_thickness, 0), Quaternion(1, 0, 0, 0), material, true, true, -20, -20);
-	InitObject(R, 100000, Vector(container_size.x - container_thickness, container_height - container_thickness, 0), Quaternion(1, 0, 0, 0), material, true, true, -20, -20);
-	InitObject(F, 100000, Vector(0, container_height - container_thickness, -container_size.z + container_thickness), Quaternion(1, 0, 0, 0), material, true, true, -20, -20);
-	InitObject(B, 100000, Vector(0, container_height - container_thickness, container_size.z - container_thickness), Quaternion(1, 0, 0, 0), material, true, true, -20, -20);
 	InitObject(Bottom, 100000, Vector(0, container_height - container_size.y, 0), Quaternion(1, 0, 0, 0), material, true, true, -20, -20);
 
 	AddCollisionGeometry(
@@ -135,21 +123,21 @@ int main(int argc, char* argv[]) {
 	//FinalizeObject(R, (ChSystemGPU *) system_gpu);
 	//FinalizeObject(F, (ChSystemGPU *) system_gpu);
 	//FinalizeObject(B, (ChSystemGPU *) system_gpu);
-	FinalizeObject(Bottom, (ChSystemGPU *) system_gpu);
+	FinalizeObject(Bottom, (ChSystemParallelDVI *) system_gpu);
 	//=========================================================================================================
 	cout << num_per_dir.x << " " << num_per_dir.y << " " << num_per_dir.z << " " << num_per_dir.x * num_per_dir.y * num_per_dir.z << endl;
 
-	ParticleGenerator layer_gen(system_gpu);
+	ParticleGenerator<ChSystemParallelDVI> layer_gen(system_gpu);
 	layer_gen.SetDensity(11340);
 	layer_gen.SetRadius(R3(particle_radius));
 	layer_gen.SetNormalDistribution(particle_radius, 1 / 300.0);
 	layer_gen.material->SetFriction(particle_friction);
 	layer_gen.material->SetCohesion(0);
 	layer_gen.material->SetCompliance(0);
-
+	layer_gen.AddMixtureType(MIX_SPHERE);
 	//layer_gen.addHCPCube(num_per_dir,1,R3(0,0,0),R3(0,0,0));
 	//num_per_dir=I3(10,10,10);
-	layer_gen.addVolume(R3(0, 2, 0), SPHERE, num_per_dir, R3(0, 0, 0));
+	layer_gen.addVolume(R3(0, 2, 0), num_per_dir, R3(0, 0, 0));
 
 	//addPerturbedLayer(R3(0, -5 + particle_radius + container_thickness, 0), ELLIPSOID, R3(particle_radius), num_per_dir, R3(.01, .01, .01), 10, 1, system_gpu);
 	//=========================================================================================================
@@ -176,11 +164,11 @@ int main(int argc, char* argv[]) {
 		current_time += timestep;
 		system_gpu->DoStepDynamics(timestep);
 		cout << "step " << i;
-		cout << " Residual: " << ((ChLcpSolverGPU *) (system_gpu->GetLcpSolverSpeed()))->GetResidual();
-		cout << " ITER: " << ((ChLcpSolverGPU *) (system_gpu->GetLcpSolverSpeed()))->GetTotalIterations();
-		cout << " OUTPUT STEP: Time= " << current_time << " bodies= " << system_gpu->GetNbodies() << " contacts= " << system_gpu->GetNcontacts() << " step time=" << system_gpu->GetTimerStep()
-				<< " lcp time=" << system_gpu->GetTimerLcp() << " CDbroad time=" << system_gpu->GetTimerCollisionBroad() << " CDnarrow time=" << system_gpu->GetTimerCollisionNarrow() << " Iterations="
-				<< ((ChLcpSolverGPU*) (system_gpu->GetLcpSolverSpeed()))->GetTotalIterations() << "\n";
+		cout << " Residual: " << ((ChLcpSolverParallelDVI *) (system_gpu->GetLcpSolverSpeed()))->GetResidual();
+		cout << " ITER: " << ((ChLcpSolverParallelDVI *) (system_gpu->GetLcpSolverSpeed()))->GetTotalIterations();
+		cout << " OUTPUT STEP: Time= " << current_time << " bodies= " << system_gpu->GetNbodies() << " contacts= " << system_gpu->GetNcontacts() << " step time="
+				<< system_gpu->GetTimerStep() << " lcp time=" << system_gpu->GetTimerLcp() << " CDbroad time=" << system_gpu->GetTimerCollisionBroad() << " CDnarrow time="
+				<< system_gpu->GetTimerCollisionNarrow() << " Iterations=" << ((ChLcpSolverParallelDVI*) (system_gpu->GetLcpSolverSpeed()))->GetTotalIterations() << "\n";
 
 		TimingFile(system_gpu, timing_file_name, current_time);
 

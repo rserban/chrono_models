f936f56bf93169f22d9210852ae1256b81360336
Hammad Mazhar <hammad.maz@gmail.com>
2014-04-08 18:56:50 -0500

    no bilateral iters

--- models/gpu/anchor.cpp
+++ models/gpu/anchor.cpp
@@ -128,7 +128,7 @@ int main(int argc, char* argv[]) {
 	((ChLcpSolverParallelDVI *) (system_gpu->GetLcpSolverSpeed()))->SetMaxIterationNormal(max_iter);
 	((ChLcpSolverParallelDVI *) (system_gpu->GetLcpSolverSpeed()))->SetMaxIterationSliding(max_iter);
 	((ChLcpSolverParallelDVI *) (system_gpu->GetLcpSolverSpeed()))->SetMaxIterationSpinning(max_iter);
-	((ChLcpSolverParallelDVI *) (system_gpu->GetLcpSolverSpeed()))->SetMaxIterationBilateral(max_iter);
+	((ChLcpSolverParallelDVI *) (system_gpu->GetLcpSolverSpeed()))->SetMaxIterationBilateral(0);
 	system_gpu->SetTol(tolerance);
 	system_gpu->SetTolSpeeds(tolerance);
 	system_gpu->SetMaxPenetrationRecoverySpeed(1e9);
@@ -318,14 +318,14 @@ int main(int argc, char* argv[]) {
 
 //=========================================================================================================
 //Rendering specific stuff:
-//	ChOpenGLManager * window_manager = new ChOpenGLManager();
-//	ChOpenGL openGLView(window_manager, system_gpu, 800, 600, 0, 0, "Test_Solvers");
-//	//openGLView.render_camera->camera_pos = Vector(0, -5, -10);
-//	//openGLView.render_camera->look_at = Vector(0, -5, 0);
-//	openGLView.render_camera->mScale = 20;
-//	openGLView.SetCustomCallback(RunTimeStep);
-//	openGLView.StartSpinning(window_manager);
-//	window_manager->CallGlutMainLoop();
+//		ChOpenGLManager * window_manager = new ChOpenGLManager();
+//		ChOpenGL openGLView(window_manager, system_gpu, 800, 600, 0, 0, "Test_Solvers");
+//		openGLView.render_camera->camera_position = glm::vec3(0, -5, -10);
+//		openGLView.render_camera->camera_look_at = glm::vec3(0, 0, 0);
+//		openGLView.render_camera->camera_scale = 2;
+//		openGLView.SetCustomCallback(RunTimeStep);
+//		openGLView.StartSpinning(window_manager);
+//		window_manager->CallGlutMainLoop();
 //=========================================================================================================
 	int file = 0;
 
